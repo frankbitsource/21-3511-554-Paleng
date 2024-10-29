d45ba4044ff8a3de6783420798605c7ef37ca7b1
unknown <frankjasper11@gmail.com>
2024-10-29 19:17:44 +0800

    Adding CRUD first video demo

---
+++ activity1-12/activity6/activity6.php
@@ -0,0 +1,15 @@
+<?php
+   
+    $favoriteMovies = [
+        "The Shawshank Redemption",
+        "Inception",
+        "The Dark Knight",
+        "Interstellar",
+        "Parasite"
+    ];
+
+   
+    foreach ($favoriteMovies as $movie) {
+        echo $movie . "<br>";  // Print each movie's name
+    }
+?>
---
+++ activity1-12/activity7/activity7.php
@@ -0,0 +1,12 @@
+<?php
+    $student = array(
+    "Name" => "Alice",
+    "Age" => 20,
+    "Grade" => "A",
+    "City" => "Baguio"
+    );
+
+    foreach ($student as $key => $value) {
+    echo "$key: $value<br>";
+    }
+?>
